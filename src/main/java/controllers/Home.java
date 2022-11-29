46ce875c04c112ec2efd78be09b8264218f4deb8
radovankrupka <rkrupka01@gmail.com>
2022-11-29 20:38:47 +0100

    basic project layout

--- src/main/java/controllers/Home.java
+++ src/main/java/controllers/Home.java
@@ -63,63 +63,6 @@ public class Home extends HttpServlet {
 
         }
 
-        /*//nie som lognuty, nechcem sa lognut
-        if (operacia == null && session.getAttribute("user") == null  ) {
-            System.out.println("CHYBA, OPERACIA = null");
-            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-            dispatcher.forward(request,response);
-        }
-
-        //som lognuty, chcem zobrazit stranku
-*//*
-        if (UserDAO.checkIfUserExists(   ((User)session.getAttribute("user") ).getLogin(),((User)session.getAttribute("user") ).getPwd()   )){
-*//*
-            if (session.getAttribute("user") != null){
-
-            List<Tovar> tovarList =  TovarDAO.getAllProducts();
-            tovarList.stream().forEach(System.out::println);
-            request.getSession().setAttribute("tovarList", tovarList);
-
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/home-page.jsp");
-            dispatcher.forward(request,response);
-
-        }
-
-        //nie som lognuty, chcem sa lognut
-        if (operacia.equals("login") && session.getAttribute("user") == null ) {// toto tereba poriesit
-            System.out.println("email : " + request.getParameter("email") + " heslo :  " + request.getParameter("password"));
-            if (UserDAO.checkIfUserExists(request.getParameter("email"), request.getParameter("password"))){
-                // USPESNE PRIHLASENIE
-                User user = UserDAO.getUserByLogin(request.getParameter("email"));
-                session.setAttribute("user", user);
-
-                List<Tovar> tovarList =  TovarDAO.getAllProducts();
-                tovarList.stream().forEach(System.out::println);
-                request.getSession().setAttribute("tovarList", tovarList);
-
-                RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/home-page.jsp");
-                dispatcher.forward(request,response);
-
-            } else { //prihlasenie nevyslo / zle data
-                //wrong credentials
-                System.out.println("wrong cred + |" + operacia + "|");
-                System.out.println("|" + request.getParameter("email") + "|" + request.getParameter("password")+"|");
-                session.invalidate();
-                RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-                dispatcher.forward(request,response);
-            }
-        }
-
-
-
-            // chcem sa odhlasit
-        if (operacia != null && operacia.equals("logout")){
-            request.getSession().invalidate();
-            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-            dispatcher.forward(request,response);
-        }
-
-        System.out.println("nic?");*/
 
 
     }
